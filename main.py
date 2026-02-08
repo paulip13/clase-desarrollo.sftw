a54fa723d13d13aa3ab11c0cda5d232c8a5df897
paulip13 <paulip2713@gmail.com>
2026-02-07 22:38:34 -0500

    feat: agregar funcion para sumar con docstring

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-def sumar_dos_numeros(numero1:int,numero2:int):
-    return numero1+numero2
+def sumar_dos_numeros(numero1: int, numero2: int) -> int:
+    """
+    Suma dos números enteros y retorna el resultado.
 
-print(sumar_dos_numeros(2,1))
+    Parámetros:
+    numero1 (int): Primer número a sumar.
+    numero2 (int): Segundo número a sumar.
+
+    Retorna:
+    int: Resultado de la suma.
+    """
+    return numero1 + numero2
+
+
+print(sumar_dos_numeros(2, 1))
